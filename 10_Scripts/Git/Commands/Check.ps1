a9b535057c4347e3c767a27b9b79477660d39691
stavanzer <noelmyint@hotmail.com>
2026-07-30 19:34:42 -0400

    fix: handle single status item in pw-git check

--- 10_Scripts/Git/Commands/Check.ps1
+++ 10_Scripts/Git/Commands/Check.ps1
@@ -55,7 +55,7 @@ function Invoke-PwGitCheck {
     }
 
     Write-Host ''
-    $status = Get-PwGitStatusLines
+    $status = @(Get-PwGitStatusLines)
     if ($status.Count -eq 0) {
         Write-Host '[ OK ] Working tree is clean.'
     }
